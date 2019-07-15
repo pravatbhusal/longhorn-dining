c1533df6be236956594b0d4fb3aca32cd73f8cee
Nikhil Kumar <31876604+nikumar1013@users.noreply.github.com>
2019-07-14 20:19:39 -0500

    Syntax formatting

--- server/scraper.py
+++ server/scraper.py
@@ -1,3 +1,6 @@
+# Authors:
+# Nikhil Kumar & Pravat Bhusal
+
 import urllib.request
 from urllib.parse import urlparse, parse_qs
 import json
@@ -9,21 +12,21 @@ def parse_meals(url):
     parsed_url = urlparse(url)
     domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_url)
 
-    # Dictionary to store the meals
-    meals = {}
+    # dictionary to store the meals
+    meals = dict()
     
-    # Makes a request to the given URL and stores the response
+    # makes a request to the given URL and stores the response
     response = urllib.request.urlopen(url)
 
-    # Reads in the response and creates a variable out of it to be used as a parser
+    # reads in the response and creates a variable out of it to be used as a parser
     html_doc = response.read()
     soup = BeautifulSoup(html_doc, 'html.parser')
-    for text in soup.find_all('div', {"class": "jumbotron"}):
+    for source_text in soup.find_all('div', {'class": "jumbotron'}):
         # map each meal's URL into the meals dictionary
-        meal = str(text.h1.strong.text)
-        meals[meal] = domain + "location?meal=" + meal
+        meal = str(source_text.h1.strong.text)
+        meals[meal] = domain + 'location?meal=' + meal
 
-    # Converts the meals dictionary into JSON
+    # converts the meals dictionary into JSON
     meals_json = json.dumps(meals)
     return meals_json
 
@@ -36,60 +39,61 @@ def parse_locations(url):
     # parse the meal from the URL
     meal = parse_qs(parsed_url.query)['meal'][0]
 
-    # Dictionary to store the locations
-    locations = {}
+    # dictionary to store the locations
+    locations = dict()
 
-    # Makes a request to the given URL and stores the response
+    # makes a request to the given URL and stores the response
     response = urllib.request.urlopen(url)
 
-    # Reads in the response and creates a variable out of it to be used as a parser
+    # reads in the response and creates a variable out of it to be used as a parser
     html_doc = response.read()
     soup = BeautifulSoup(html_doc, 'html.parser')
-    for text in soup.find_all('div', {"class": "jumbotron"}):
+    for source_text in soup.find_all('div', {'class': 'jumbotron'}):
         # map each location URL into the locations dictionary
-        location = str(text.h1.strong.text).replace(" ", "%20")
-        locations[location] = domain + "select?meal=" + meal + "&loc=" + location
+        location = str(source_text.h1.strong.text).replace(' ', '%20')
+        locations[location] = domain + 'select?meal=' + meal + '&loc=' + location
 
-    # Converts the locations dictionary into JSON
+    # converts the locations dictionary into JSON
     locations_json = json.dumps(locations)
     return locations_json
 
 # parse a URL menu
 def parse_menu(url):
-    # Makes a request to the given URL and stores the response
+    # makes a request to the given URL and stores the response
     response = urllib.request.urlopen(url)
 
-    # Reads in the response and creates a variable out of it to be used as a parser
+    # reads in the response and creates a variable out of it to be used as a parser
     html_doc = response.read()
     soup = BeautifulSoup(html_doc, 'html.parser')
 
-    # Process line function
+    # process line function
     def process_line(line, start_char, end_char, start_shift, end_shift):
         start = line.find(start_char)
         end = line.find(end_char)
         result = line[start + start_shift : end - end_shift]
         return result
 
-    # Dictionary to store the food names as keys and a list of their dietary icon url's as values
-    categories_to_names_and_images = dict()
+    # dictionary to store the food names as keys and a list of their dietary icon url's as values
+    menu = dict()
 
-    # Finds all the td tags in the HTML text
-    for text in soup.find_all('td'):  
-        # Splits the HTML text into separate lines of text
-        lines = str(text).split('\n')
+    # finds all the td tags in the HTML text
+    for source_text in soup.find_all('td'):  
 
-        # Parses the HTML table line by line
+        # splits the HTML text into separate lines of text
+        lines = str(source_text).split('\n')
+
+        # parses the HTML table line by line
         for line in lines:
             if line.startswith('<td class="name lead station'):
                 category = process_line(line, ';', '</td>', 3, 0)
-                categories_to_names_and_images[category] = dict()
+                menu[category] = dict()
             if line.startswith('<span>') and 'Calories' not in line:
                 food_name = process_line(line, '>', '</', 1, 0)
-                categories_to_names_and_images[category][food_name] = list()
+                menu[category][food_name] = list()
             if line.startswith('<img alt='):
                 img_url = process_line(line, 'src=', 'width', 5, 2)
-                categories_to_names_and_images[category][food_name].append(img_url)
+                menu[category][food_name].append(img_url)
 
-    # Converts the menu dictionary into JSON
-    menu_json = json.dumps(categories_to_names_and_images)
+    # converts the menu dictionary into JSON
+    menu_json = json.dumps(menu)
     return menu_json
