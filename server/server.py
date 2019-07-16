28743d1f68a9268b0ddcba95e9a6909d93222f70
Nikhil Kumar <31876604+nikumar1013@users.noreply.github.com>
2019-07-15 23:59:37 -0500

    Added scraper imports and routing functions

--- server/server.py
+++ server/server.py
@@ -1,14 +1,32 @@
+# Author: 
+# Nikhil Kumar
+
 # server imports
 from flask import Flask, request
 server = Flask(__name__)
 
 # scraper import
+from scraper import parse_meals, parse_locations, parse_menu
 
-
-# landing route
+# landing route (to be removed)
 @server.route("/")
 def landing():
     return "<h1>Landing Page of server.</h1>"
+
+# meal selection route
+@server.route("/meal-selection")
+def meal_selection():
+    return parse_meals
+
+# locations route
+@server.route("/meal-selection/locations")
+def locations():
+    return parse_locations
+
+# menu route
+@server.route("/meal-selection/locations/menu")
+def menu():
+    return parse_menu
 
 if __name__ == "__main__":
     server.run("localhost", 8080)
