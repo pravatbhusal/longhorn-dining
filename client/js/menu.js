40539219e0bfc26c3ff7b269e586726d7c86f188
Shadowsych <pravat.bhusal@gmail.com>
2019-07-18 22:28:47 -0600

    Position the filters and the menu items in CSS

--- client/js/menu.js
+++ client/js/menu.js
@@ -22,10 +22,59 @@ fetch(serverURL + "/meal/location/menu", {
   })
 }).then((data) => {
   data.json().then((json) => {
-    // receive each individual items
-    console.log(json)
+    // parse the food information
+    addFilterButtons(json["Filters"]);
+    const menu = json["Menu"];
+    const nutrition = json["Nutrition"];
   });
 }).catch((error) => {
   // an error occurred when fetching the data
   console.log(error);
 });
+
+// add the filter buttons nto the content
+function addFilterButtons(filters) {
+  // loop through each filter from the JSON data
+  Object.keys(filters).forEach((filter) => {
+    // create a button div
+    const filterDiv = document.createElement("div");
+    filterDiv.id = "filter";
+
+    // the inner html to the button div
+    const upperCaseFilter = getFilterName(filter).toUpperCase();
+    const filterImage = filters[filter];
+    filterDiv.innerHTML = `
+      <div id="filter-container">
+        <img id="filter-image" src="${filterImage}">
+        <p id="filter-text">${upperCaseFilter}</p>
+      </div>
+    `;
+
+    // append the filter div into the filters tag
+    document.getElementById("filters").appendChild(filterDiv);
+  });
+}
+
+// return special cases for filters
+function getFilterName(filter) {
+  let filterName = filter;
+
+  // determine the filter name from the cases
+  if(filter != "Vegan" && filter != "Healthy") {
+    if(filter == "Veggie") {
+      // special case for vegetarian renaming
+      filterName = "Vegetarian";
+    } else {
+      // special cases for plurals and renaming
+      if(filter == "Milk") {
+        filter = "Dairy";
+      } else if(filter == "Nuts") {
+        filter = "Nut";
+      } else if(filter == "Eggs") {
+        filter = "Egg";
+      }
+      filterName = filter + "-Free";
+    }
+  }
+  return filterName;
+}
